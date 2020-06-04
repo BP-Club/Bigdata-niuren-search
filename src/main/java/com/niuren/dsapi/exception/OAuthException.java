0a81c2965c00b0be33c53803e15320c545d21b72
dailinwei <dailinwei@tcpjw.org>
2020-06-04 14:25:23 +0800

    create

--- src/main/java/com/niuren/dsapi/exception/OAuthException.java
+++ src/main/java/com/niuren/dsapi/exception/OAuthException.java
@@ -3,7 +3,7 @@ package com.niuren.dsapi.exception;
 /**
  * @author: dailinwei
  * @description: 表示授权过程抛出的异常
- * @create: 2018-05-02 11:14
+ * @create: 2018-05-02 11:15
  **/
 public class OAuthException extends Exception {
 
